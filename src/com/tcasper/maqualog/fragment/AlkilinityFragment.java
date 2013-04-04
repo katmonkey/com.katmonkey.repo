32074f12b03f835771ddd3b89fc7fe90c9a3fcde
Tom Casper <tmcasper@gmail.com>
2013-04-04 15:31:16 -0400

    Adding new files

---
+++ src/com/tcasper/maqualog/fragment/AlkilinityFragment.java
@@ -0,0 +1,25 @@
+package com.tcasper.maqualog.fragment;
+
+import com.tcasper.maqualog.R;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+
+public class AlkilinityFragment extends Fragment {
+	
+	public static final String ARG_SECTION_NUMBER = "section_number";
+
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		if(container == null) {
+			return null;
+		}
+		return (RelativeLayout)inflater.inflate(R.layout.alkilinity_fragment, container, false);
+	}
+
+}
