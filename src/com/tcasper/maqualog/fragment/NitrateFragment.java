6e4b9f5715a5ab8f1e429c7d8ce9f47f91da1d3a
Tom Casper <tmcasper@gmail.com>
2013-04-08 16:42:52 -0400

    Added db functionality for more tests

--- src/com/tcasper/maqualog/fragment/NitrateFragment.java
+++ src/com/tcasper/maqualog/fragment/NitrateFragment.java
@@ -1,25 +1,57 @@
 package com.tcasper.maqualog.fragment;
 
-import com.tcasper.maqualog.R;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
+import com.tcasper.maqualog.R;
+import com.tcasper.maqualog.db.MaquaLogOpenHelper;
+
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RelativeLayout;
 
 public class NitrateFragment extends Fragment {
 	
 	public static final String ARG_SECTION_NUMBER = "section_number";
 
+	Button nitrateSubmitButton;
+	EditText nitrateEditText;
+	MaquaLogOpenHelper dbHelper;
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		if(container == null) {
 			return null;
 		}
-		return (RelativeLayout)inflater.inflate(R.layout.nitrate_fragment, container, false);
-	}
+		View nitrateView = inflater.inflate(R.layout.nitrate_fragment, container, false);
+		nitrateSubmitButton = (Button)nitrateView.findViewById(R.id.nitrateSubmitButton);
+		nitrateEditText = (EditText)nitrateView.findViewById(R.id.nitrateEditText);
+		nitrateSubmitButton.setOnClickListener(new OnClickListener() {
+			
+			public void onClick(View v) {
+				Log.d("NitrateFragment", "Button clicked!!!");
+				DateFormat sdf = SimpleDateFormat.getDateTimeInstance();
+				String editTextValue = nitrateEditText.getText().toString();
+				ContentValues vals = new ContentValues();
+				vals.put("nitrate", editTextValue);
+				vals.put("date", sdf.format(Calendar.getInstance().getTime()));
+				dbHelper = MaquaLogOpenHelper.getInstance(getActivity().getApplicationContext());
+				SQLiteDatabase db = dbHelper.getWritableDatabase();
+				//dbHelper.close();
+				db.insert("test_entries", "", vals);
+			}
+		});
 
+		return nitrateView;
+	}
 }
--- src/com/tcasper/maqualog/fragment/NitriteFragment.java
+++ src/com/tcasper/maqualog/fragment/NitriteFragment.java
@@ -1,25 +1,59 @@
 package com.tcasper.maqualog.fragment;
 
-import com.tcasper.maqualog.R;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
+import com.tcasper.maqualog.R;
+import com.tcasper.maqualog.db.MaquaLogOpenHelper;
+
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RelativeLayout;
 
 public class NitriteFragment extends Fragment {
 	
 	public static final String ARG_SECTION_NUMBER = "section_number";
 
+	Button nitriteSubmitButton;
+	EditText nitriteEditText;
+	MaquaLogOpenHelper dbHelper;
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		if(container == null) {
 			return null;
 		}
-		return (RelativeLayout)inflater.inflate(R.layout.nitrite_fragment, container, false);
+		View nitriteView = inflater.inflate(R.layout.nitrite_fragment, container, false);
+		nitriteSubmitButton = (Button)nitriteView.findViewById(R.id.nitriteSubmitButton);
+		nitriteEditText = (EditText)nitriteView.findViewById(R.id.nitriteEditText);
+		nitriteSubmitButton.setOnClickListener(new OnClickListener() {
+			
+			public void onClick(View v) {
+				Log.d("NitriteFragment", "Button clicked!!!");
+				DateFormat sdf = SimpleDateFormat.getDateTimeInstance();
+				String editTextValue = nitriteEditText.getText().toString();
+				ContentValues vals = new ContentValues();
+				vals.put("nitrite", editTextValue);
+				vals.put("date", sdf.format(Calendar.getInstance().getTime()));
+				dbHelper = MaquaLogOpenHelper.getInstance(getActivity().getApplicationContext());
+				SQLiteDatabase db = dbHelper.getWritableDatabase();
+				//dbHelper.close();
+				db.insert("test_entries", "", vals);
+			}
+		});
+		
+		
+		return (RelativeLayout)nitriteView;
 	}
 
 }
