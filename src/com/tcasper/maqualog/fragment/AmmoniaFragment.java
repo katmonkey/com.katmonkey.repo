56ecd77364b447e842cf690bc66236e022889f6a
Tom Casper <tmcasper@gmail.com>
2013-04-12 16:13:02 -0400

    adding dynamic tablerow funcionality

--- src/com/tcasper/maqualog/fragment/AmmoniaFragment.java
+++ src/com/tcasper/maqualog/fragment/AmmoniaFragment.java
@@ -10,6 +10,7 @@ import com.tcasper.maqualog.db.MaquaLogOpenHelper;
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
@@ -22,6 +23,10 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RelativeLayout;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TableRow.LayoutParams;
+import android.widget.TextView;
 
 public class AmmoniaFragment extends Fragment {
 	
@@ -63,5 +68,31 @@ public class AmmoniaFragment extends Fragment {
 			}
 		});
 		return ammoniaView;
+	}
+	
+	
+	
+	private void addHistoryRow(View v) {
+		LayoutInflater inflater = (LayoutInflater)getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		View ammoniaView = inflater.inflate(R.layout.ammonia_fragment, null);
+		TableLayout ammoniaTableLayout = (TableLayout)ammoniaView.findViewById(R.id.ammoniaTable);
+  	  	TableRow ammoniaTableRow = new TableRow(getActivity().getApplicationContext());
+  	  	LayoutParams ammoniaLayoutParams = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
+  	  	ammoniaTableRow.setLayoutParams(ammoniaLayoutParams);
+
+  	  	TextView date = new TextView(getActivity().getApplicationContext());
+  	  	TextView value = new TextView(getActivity().getApplicationContext());
+  	  	date.setLayoutParams(ammoniaLayoutParams);
+  	  	value.setLayoutParams(ammoniaLayoutParams);
+  	  	
+  	  	date.setBackgroundColor(Color.WHITE);
+  	  	value.setBackgroundColor(Color.WHITE);
+  	  	date.setText(""/* Get this value from DB */);
+  	  	value.setText(""/* Get this value from DB */);
+
+  	  	ammoniaTableRow.addView(date);
+  	  	ammoniaTableRow.addView(value);
+
+  	  	ammoniaTableLayout.addView(ammoniaTableRow, new TableLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
 	}
 }
